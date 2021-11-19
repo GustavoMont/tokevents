b8e1b1e889ddc7f0c69bf0f48a6ccebed7d4a48b
GustavoMont <lgmluisgm@gmail.com>
2021-11-19 08:23:36 -0300

    :pencil: move index path

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv/config')
+
+const cadastroRoute = require('./routes/cadastro')
+
+const app = express()
+
+//app.use(express.static('../../frontend'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const pathsHtml = `
+<h1>Certo Chefe</h1>
+<a href="/cadastro">Cadastro</a>
+`
+
+mongoose.connect(process.env.DATA_BASE_CONNECTION, { useNewUrlParser: true }, () =>{
+    console.log('Conectado')
+})
+
+app.get('/', (req, res) =>{
+    res.send(pathsHtml)
+})
+
+app.use('/cadastro', cadastroRoute)
+
+
+
+app.listen(8080, () =>{
+    console.log('Server is runing')
+})
