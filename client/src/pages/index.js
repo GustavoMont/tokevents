e41e4ae39e3f95389bec40b334acade34c9b560c
GustavoMont <lgmluisgm@gmail.com>
2021-11-21 00:21:27 -0300

    :pencil: create Form Component

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Form from '../Components/Form'
 
 export default function Home(){
+    const login = [
+        {label: 'Usuário ou Email', name: 'login', type: 'text'},
+        {label: 'Senha', name: 'senha', type: 'password'},
+    ]
+    const cadastro = [
+        {label: 'Email', name: 'email', type: 'email'},
+        {label: 'User Name', name: 'user_name', type: 'text'},
+        {label: 'Nome', name: 'user_name', type: 'text'},
+        {label: 'Senha', name: 'password', type: 'password'},
+        {label: 'Confirmar senha', name: 'confirm', type: 'password'},
+    ]
+
     return (
         <>
-            <h1>FIRST PAGE</h1>
+            <section>
+                <Form inputs={login} method={"POST"} action="localhost:8080/auth/login" butao="Login" />
+                <hr />
+                <Form inputs={cadastro} method={"POST"} action="localhost:8080/auth/signIn" butao="Cadastrar" />
+                
+            </section>
         </>
     )
 }
