4645d9560a734e59a6766714d31435eac3ebe0ed
GustavoMont <lgmluisgm@gmail.com>
2021-11-21 14:24:54 -0300

    :pencil: Create Post it component

---
+++ client/src/Components/PostIt.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+export default function PostIt(){
+    return (
+        <div>
+            <h4>EVENT TITLE</h4>
+            <p>Little description</p>
+            <p>DATA</p>
+        </div>
+    )
+}
