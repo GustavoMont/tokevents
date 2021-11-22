e447723f57145441f133cb5b17daeb91824102e1
GustavoMont <lgmluisgm@gmail.com>
2021-11-22 09:23:46 -0300

    :pencil: make code easier to understand

--- client/src/utils/randomGenarate.js
+++ client/src/utils/randomGenarate.js
@@ -1,1 +1,3 @@
-export const randomBg = (min, max) => Math.floor(Math.random() * ((max) - min + 1)) + min
+const randomBetween = (min, max) => Math.floor(Math.random() * ((max) - min + 1)) + min
+
+export const randomBg = (colors ,min, max) => colors[randomBetween(min, max)]
