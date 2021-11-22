84d98d0916d1cfecadfa6c676fcd3e17cd033433
GustavoMont <lgmluisgm@gmail.com>
2021-11-22 20:25:07 -0300

    :pencil: use context to make private routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,15 +1,33 @@
-import { 
+import {
     Routes as Manager,
-    Route
+    Route,
+    Navigate
 } from 'react-router-dom'
 import Index from './pages'
 import Home from './pages/Home'
+import { Context } from './Context/AuthContext'
+import { useContext } from 'react'
 
-const Routes = () => (
-     <Manager>
-         <Route path="/" element={<Index />}  />
-         <Route path="/home" element={<Home />}  />
-     </Manager>
+
+const Private = ( { isAuth, children } ) => (
+    isAuth ? (
+        children
+    ) : (
+        <Navigate to="/" />
+    )
 )
+const Routes = () => {
+    const { isAuth  } = useContext(Context)
+    return (
+            <Manager>
+                <Route path="/" element={<Index />} />
+                <Route path="/home" element={
+                    <Private isAuth={isAuth} >
+                        <Home />
+                    </Private>
+                } />
+            </Manager>
+    )
+}
 
 export default Routes
