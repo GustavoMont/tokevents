1f072f2c9867a4c56f8fde6453ebd07e49a2423a
GustavoMont <lgmluisgm@gmail.com>
2021-11-23 10:29:11 -0300

    :pencil: get date from api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,41 +2,50 @@ import React, { useEffect, useState } from "react";
 import Add from "../Components/Add";
 import { randomBg } from '../utils/randomGenarate';
 import PostIt from '../Components/PostIt'
+import { formateDate } from '../utils/formateDate'
 import '../styles/Home.css'
 
-export default function Home(){
+export default function Home() {
     const bgColors = ['--postit-yellow', '--postit-pink', '--postit-green', '--postit-orange']
-    
+
     const [eventos, setEventos] = useState([])
 
-    useEffect(() =>{
-        (async () =>{
-            const eventosRes = await fetch('/events',{
-                method: 'POST',
-                headers:{
-                    'Content-Type': 'application/json',
-                    body:{
 
-                    }
-                }
+    useEffect(() => {
+        (async () => {
+            const { token } = JSON.parse(sessionStorage.getItem('@tokevents'))
+            const body = JSON.stringify({ user_id: token })
+            const eventosRes = await fetch('/events', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'auth': `Bearer ${token}`
+                },
+                body
             })
+            const { events } = await eventosRes.json()
+            if (eventos.erro) {
+                window.location.href = '/'
+                return
+            };
+            setEventos(events)
         })()
-    },[])
+    }, [])
 
     eventos.forEach(evento => {
-        evento.color = randomBg(bgColors ,0, bgColors.length-1)
+        evento.color = randomBg(bgColors, 0, bgColors.length - 1)
     })
 
     return (
         <main id="home">
             <h1>HOME PAGE</h1>
             <div id="board">
-            {eventos.map(evento => (
-                <PostIt title={evento.title} description={evento.description} 
-                    data_inicio={evento.data_inicio} data_fim={evento.data_fim} color={evento.color} 
-                />
-            ))}
-        </div>
+                {eventos.length === 0 ? 'NENHUM EVENTO ADICIONADO' : eventos.map(evento => (
+                    <PostIt title={evento.title} description={evento.description} key={evento._id}
+                        data_inicio={formateDate(evento.data_inicio)}   data_fim={formateDate(evento.data_fim)} color={evento.color}
+                    />
+                ))}
+            </div>
             <Add />
         </main>
     )
