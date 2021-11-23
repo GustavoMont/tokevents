19018b218eb00aea0b31bf429c839811feb6eee7
GustavoMont <lgmluisgm@gmail.com>
2021-11-22 21:49:21 -0300

    :pencil: fix loading bug

--- client/src/Context/AuthContext.js
+++ client/src/Context/AuthContext.js
@@ -5,14 +5,23 @@ const Context = createContext()
 
 function AuthProvider({children}){
     const [isAuth, setIsAuth] = useState(false)
+    const [loading, setLoading] = useState(true)
     useEffect(() =>{
         const session  = JSON.parse(sessionStorage.getItem('@tokevents'))
+        setLoading(true)
         if (session) {
             (async ()=>{
-                setIsAuth(await handleToken(session.token))
+                setIsAuth(await handleToken('Bearer ' + session.token))
+                setLoading(false)
             })()
+        }else{
+            setLoading(false)
         }
     }, [])
+
+    if (loading) {
+        return(<h1>Loading</h1>)
+    }
 
     return (
         <Context.Provider value=  { {isAuth} } >
