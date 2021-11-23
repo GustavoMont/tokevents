d3ac0c9127607fdccf4f279c7bc51a8163a9e1a2
GustavoMont <lgmluisgm@gmail.com>
2021-11-22 21:51:20 -0300

    :ppencil: update submit functions

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import Form from '../Components/Form'
-
+import { handleLogin, handleSignIn } from '../utils/handleAuth'
 
 export default function Index(){
     const login = [
         {label: 'Usuário ou Email', name: 'login', type: 'text', key: 'login_login'},
-        {label: 'Senha', name: 'senha', type: 'password', key: 'login_senha'},
+        {label: 'Senha', name: 'password', type: 'password', key: 'login_password'},
     ]
     const cadastro = [
         {label: 'Email', name: 'email', type: 'email', key: 'cad_email'},
         {label: 'User Name', name: 'user_name', type: 'text', key: 'cad_user_name'},
         {label: 'Nome', name: 'nome', type: 'text', key: 'cad_nome'},
         {label: 'Senha', name: 'password', type: 'password', key: 'cad_pass'},
-        {label: 'Confirmar senha', name: 'confirm', type: 'password', key: 'cad_confirm'},
+        {label: 'Confirmar senha', name: 'confirm', type: 'password', key:'cad_confirm'},
     ]
+
 
     return (
         <>
             <section>
-                <Form inputs={login} method={"POST"} action="/auth/login" butao="Login" />
+                <Form inputs={login} method={"POST"} action="/auth/login" butao="Login" submit={handleLogin} />
                 <hr />
-                <Form inputs={cadastro} method={"POST"} action="/auth/signIn" butao="Cadastrar" />
+                <Form inputs={cadastro} method={"POST"} action="/auth/signIn" butao="Cadastrar" submit={handleSignIn} />
                 
             </section>
         </>
