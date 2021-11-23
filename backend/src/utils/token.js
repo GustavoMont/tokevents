762d0db100de9c92600c01c719f9f37f67650944
GustavoMont <lgmluisgm@gmail.com>
2021-11-23 01:25:30 -0300

    :pencil: encrypt all user info

--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 require('dotenv/config')
 
-const generateToken = (user_id) => jwt.sign({user_id}, process.env.TOKEN_KEY, {
+const generateToken = (nome, user_name, id) => jwt.sign({nome, user_name, id}, process.env.TOKEN_KEY, {
         expiresIn: 60*60*8
     })
 
