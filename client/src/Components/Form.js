f2834fc1ad422fb66db009df70aa37577bfe74db
GustavoMont <lgmluisgm@gmail.com>
2021-11-22 21:52:18 -0300

    :pencil: using handle functions

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 
 
-export default function Form( { inputs, butao, action, method, submit } ){
+export default function Form({ inputs, butao, action, method, submit }) {
     return (
         <>
-            <form action={action} method={method} onSubmit={submit} >
+            <form action={action} method={method} onSubmit={submit}> 
                 {inputs.map((input) => (
                     <div className="form-field" key={input.key} >
                         <label htmlFor={input.name}>{input.label}: </label>
-                        <input type={input.type} name={input.name} id={input.name}  required />
+                        <input type={input.type} name={input.name} id={input.name} required />
                     </div>
                 ))}
 
